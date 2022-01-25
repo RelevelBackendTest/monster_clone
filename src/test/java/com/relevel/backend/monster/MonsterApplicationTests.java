5d4e5074fa81fe3ce28f52bcaa483dedb418f600
RelevelBackendTest <87223414+RelevelBackendTest@users.noreply.github.com>
2022-01-25 21:44:06 +0530

    Add files via upload

---
+++ src/test/java/com/relevel/backend/monster/MonsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevel.backend.monster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
